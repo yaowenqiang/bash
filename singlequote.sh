a94cf54f7c25ca444e5ca47dfe7a8e72348bae6a
yaowenqiang <yaowenqiang111@163.com>
2015-02-07 04:21:22 +0800

    add the singlequote script file.

---
+++ singlequote.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "why can't i write 's between single quotes"
+echo
+
+# 一种绕弯的办法
+#echo 'Why can'\'''"'"'s between single quotes'
+echo 'Why can'\''t I write'"'"' s between single quotes'
+#
+# 三个被单引号引用的字符串，在这三个字符串之间有一个用转义符转义的单引号，和一个用双绰号卑鄙起来的单引号.
+
