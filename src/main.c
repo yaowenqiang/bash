9c9be6b77dde2d85232028ee5cb073cd670c5938
yaowenqiang <yaowenqiang111@163.com>
2015-02-07 05:10:33 +0800

    change the main file

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include "version.h"
 #include <stdio.h>
-int main(){
-    printf("Hello world.\n");
-    printf("version %s.\n",_VERSION);
+
+int
+main()
+{
+    printf( "Hello, world.\n" );
+    printf( "version: %s.\n", _VERSION );
     return 0;
 }
-
