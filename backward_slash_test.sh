ede29398d6c6b2236b7ad57f22f00de199fa0329
yaowenqiang <yaowenqiang111@163.com>
2015-02-13 19:06:16 +0800

    添加了变量引用转义的示例

--- backward_slash_test.sh
+++ backward_slash_test.sh
@@ -36,3 +36,22 @@ EOF
 # variable=\
 # echo "$variable"
 # 不能正常运行－会报错
+# 一个"裸体的" 转义符是不能够安全的赋值给变量的
+# 事实上在这里"\" 转义了一个换行符(变成了续行符的含义)
+#+效果就是                              variable=echo "$variable"
+#+                                      不可用的变量赋值
+variable=\ 
+#           \^转义一个空格
+echo "$variable"                        # 显示空格
+
+variable=\\
+echo "$variable"                        # \
+# variable=\\\
+# echo "$variable"
+# 不能正常运行 - 报错
+# 第一个转义符把第2个\转义了，但是第3个又变成了"裸体"的了
+variable=\\\\
+echo "$variable"                        # \\
+                                        # 第2个和第4个反斜线被转义了 
+                                        # 这里是正确的
+exit 0
