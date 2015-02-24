4b01de2c68e4964dfa4d446da2264b728046bf8d
yaowenqiang <yaowenqiang111@163.com>
2015-02-24 23:15:26 +0800

    中括号中和条件判断不一定非要if,可以使用列表结构

--- test_and_square_bracket.sh
+++ test_and_square_bracket.sh
@@ -52,4 +52,10 @@ if cd "$dir" 2>/dev/null;then       # 2>/dev/null会隐藏错误信息
 else
     echo "Can't change to $dir."
 fi
+
+var1=20
+var2=22
+[ "$var1" -ne "$var2" ] && echo "$var1 is not equal to $var2"
+home=/home/bozo
+[ -d "$home" ] || echo "$home directory is not exists."
 exit 0
