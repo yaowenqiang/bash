f7522275021fae85e0f15871ca03047dbaefb1e8
yaowenqiang <yaowenqiang111@163.com>
2015-02-24 22:53:12 +0800

    单方括号测试对比

--- test_and_square_bracket.sh
+++ test_and_square_bracket.sh
@@ -18,3 +18,14 @@ fi
 
 echo
 
+if  [ -z "$1" ]; then                   # 与上边的代码块作用相同
+    # if [ -z "$1"                      # 应该能够运行，但是
+    #+ Bash会报错，提示缺少关闭条件测试的右中括号。
+     echo "No command-line arguments"
+ else
+     echo "First command-line argument is $1."
+fi
+
+echo
+
+
