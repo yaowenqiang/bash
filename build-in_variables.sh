8391172a6acbe0e356b1db2b142b65f0452c81cd
yaowenqiang <yaowenqiang111@163.com>
2015-03-01 16:50:08 +0800

    添加了脚本运行时间计数

--- build-in_variables.sh
+++ build-in_variables.sh
@@ -163,3 +163,6 @@ echo "Deleting stale files in $TargetDirectory"
 # echo
 
 # $REPLY     当没有参数变量提供给read命令的时候，这个变量会作为默认变量会作为默认变量提供给read命令,也可以用于select菜单,但是只提供所选择变量的编写，而不是变量本身
+
+# $SECONDS        这个脚本已经运行的时间(以秒为单位)
+
