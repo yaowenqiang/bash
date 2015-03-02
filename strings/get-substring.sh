a057897417593bd32198fd8b2843a45c0dbb73ec
yaowenqiang <yaowenqiang111@163.com>
2015-03-03 01:32:15 +0800

    提取子串

---
+++ strings/get-substring.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# get-substring.sh
+
+# ${string:position}
+# 在$string中从位置position开始提取子串
+# 如果$string是'*'或者'@',那么将会提取从位置$position开始的位置参数
+
+# ${string:position:length}
+# 在$string中从位置$position开始提取$length长度的子串
+
+stringZ=abcABC123ABCabc
+
+#       0123456789...
+#       0-based indexing.
+
+echo ${stringZ:0}      # abcabc123abcabc
+echo ${stringZ:1}      # bcabc123abcabc
+echo ${stringZ:7}      # 23abcabc
+
+
+echo ${stringZ:7:3}    # 23a
+                       # 提取子串长度为3
+
+# TODO
+# 能不能从字符串的右边(也就是结尾)部分开始提取子串?
+
+
+# TODO 这个地方没看懂
+echo ${stringZ:-4}
+# 默认是提取整个字符串，就像${parameter:-default}一样，，
+# 然而...
+
+echo ${stringZ:(-4)}        # Cabc
+echo ${stringZ: -4}         # Cabc
+# 这样，它就可以工作了.
+# 使用圆括号或者添加一个空格可以"转义"这个位置参数。 
+
