f6543c7205205cbe8ffdcd82e2c361cbeea7ce56
yaowenqiang <yaowenqiang111@163.com>
2015-03-08 23:44:49 +0800

    更多使用模式匹配示例

--- parameters/var-ext.sh
+++ parameters/var-ext.sh
@@ -34,6 +34,36 @@ echo
 #--------------------------------------------------------------------------------
 path_name=/home/boro/ideas/thoughts.for.today
 #--------------------------------------------------------------------------------
-echo "path_name = $path_name"
+echo "$path_name = $path_name"
 t=${path_name##/*/}
-echo "path_name,stripped of prefixes = $t"
+echo "$path_name,stripped of prefixes = $t"
+# 在这个特例中，与      t=`basename $path_name`    效果相同。
+# t=${path_name%/};t=${t##*/}    是更一般的解决方法。TODO,需要实际测试一下。
+#+ 但有时会失败。
+# 如果path_name 以一个换行结尾的话，那么      `basename $path_name`就不能正常工作了。
+#+ 但上边的表达式可以。
+
+t=${path_name%/*.*}
+# 与t=`dirname $path_name`效果相同
+echo "$path_name,stripped of suffixes  = $t"
+# 在某些情况下将失败，比如"../","/foo////",#"foo/","/",TODO 还末验证。
+# 删除后缀，尤其是在basename没有后缀的情况下，
+#+便是dirname可以， 不过这同时也使问题复杂化了。
+
+echo
+
+t=${path_name:11}
+echo "$path_name, with first 11 chars stripped of = $t"
+t=${path_name:11:5}
+echo "$path_name, with first 11 chars stripped of,length 5 = $t"
+echo 
+
+t=${path_name/boro/clown}
+echo "$path_name, with \"boro\" replaced by \"clown\" = $t"
+t=${path_name/today/}
+echo "$path_name,with \"today\" deleted = $t"
+t=${path_name//o/O}
+echo "$path_name,with all o's capticalized = $t"
+t=${path_name//o/}
+echo "$path_name,with all o's deleted = $t"
+exit 0
