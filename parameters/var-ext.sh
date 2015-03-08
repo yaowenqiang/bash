597e29c5a8b42a5a107162ad0f4d364191fdf3b5
yaowenqiang <yaowenqiang111@163.com>
2015-03-08 23:02:54 +0800

    变量扩展/子串替换说明

--- parameters/var-ext.sh
+++ parameters/var-ext.sh
@@ -2,3 +2,15 @@
 # var-ext.sh
 # 变量扩展/子串扩展
 
+# $(var:pos)
+# 变量var从位置开始扩展(译者注：也就是pos之前的字符都丢弃)
+# $(var:pos:len)
+# 变量var从pos开始，并扩展len个字符
+# ${var/Pattern/Replacement}
+# 使用Replacement来替换变量var中第一个匹配Pattern的字符串
+# 如果省略Replacement，那么第一个匹配Pattern的字符串将会被替换为空，也就是被删除了。
+# ${var//Pattern/Replacement}
+# 全局替换，所有在变量var匹配Pattern的字符串，都会被替换为Replacement
+# 和上边一样，如果省略Replacement, 那么所有匹配Pattern的字符串，都将被替换为空，也就是被删除掉。
+
+
