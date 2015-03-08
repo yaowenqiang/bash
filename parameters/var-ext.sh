4ff26ec314329c3d25810b1f2a2b50eb4b061232
yaowenqiang <yaowenqiang111@163.com>
2015-03-08 23:26:34 +0800

    添加了使用模式匹配来解析任意字符串的示例

--- parameters/var-ext.sh
+++ parameters/var-ext.sh
@@ -13,4 +13,27 @@
 # 全局替换，所有在变量var匹配Pattern的字符串，都会被替换为Replacement
 # 和上边一样，如果省略Replacement, 那么所有匹配Pattern的字符串，都将被替换为空，也就是被删除掉。
 
+var1=abcd-1234-defg
+echo "var1 = $var1"
 
+t=${var1#*-*}
+echo "$var1 (with everything,up to and including first - stripped out) = $t"
+#+ t=${var#*-}也一样，
+#+ 因为#匹配最短的字符串，
+#+ 同时*匹配任意前缀，包括空字符串。
+
+t=${var##*-*}
+echo "If var1 contains a \"-\",returns empty string...  var1 = $t"
+#+ 全部匹配并删除
+
+t=${var%*-*}
+echo "$var1 (with everything from the last - on stripped out) = $t"
+# TODO 输出为空，应该是全部被删除了，不是100%确定，后面在研究一下。
+
+echo
+#--------------------------------------------------------------------------------
+path_name=/home/boro/ideas/thoughts.for.today
+#--------------------------------------------------------------------------------
+echo "path_name = $path_name"
+t=${path_name##/*/}
+echo "path_name,stripped of prefixes = $t"
