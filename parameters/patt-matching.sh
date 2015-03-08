296684ffdf68cbab51bdd72da2a565c4a6c78e77
yaowenqiang <yaowenqiang111@163.com>
2015-03-08 22:22:23 +0800

    a*c匹配

--- parameters/patt-matching.sh
+++ parameters/patt-matching.sh
@@ -11,3 +11,8 @@ echo "var1 = $var1"     # abcd12345abc6789
 echo "var1 = ${var1}"   # abcd12345abc6789
                         # 另一种形式
 echo "Number of characters in ${var1} = ${#var1}"
+echo
+
+echo "pattern1 = ${pattern1}"           # a*c    (匹配'a''c'之间的任意字符)
+echo "---------------"
+echo '${var1#$pattern1} = ' "${var1#$pattern1}" # d12345abc6789
