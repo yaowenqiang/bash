9e96dfd10907005e31a41b7a9a18c373c39caaff
yaowenqiang <yaowenqiang111@163.com>
2015-03-10 01:20:45 +0800

    变量间接引用示例

--- parameters/ind-ref.sh
+++ parameters/ind-ref.sh
@@ -14,7 +14,23 @@ echo "Now a = $a"           # 现在a = z
 echo
 
 # 现在，让我们试试修改第二个引用的值。
+t=table_cell_3
+table_cell_3=24
+echo "\"table_cell_3\" = $table_cell_3"     # "table_cell_3" = 24
+echo -n "dereferenced \"t\" = ";eval echo \$$t      # 解引用 "t" = 24
+# 在这个简单的例子中，下面的表达式也能正常工作么？(为什么？).
+# eval t=\$$t;echo "\"t\" = $t."        能正常工作，不过不是非常明白 TODO
 
+echo
 
+t=table_cell_3
+NEW_VAL=387
+table_cell_3=$NEW_VAL
+echo "Changing value of \"table_cell_3\" to $NEW_VAL."
+echo "\"table_cell_3\" new $table_cell_3"
+echo -n "dereferenced \"t\" new ";eval echo \$$t
+# "eval" 带有两个参数，"echo" 和 "\$$t"(与$table_cell_3等价)
 
+# 另一个方法是使用${!t}符号 TODO 还没尝试过
+exit 0
 
