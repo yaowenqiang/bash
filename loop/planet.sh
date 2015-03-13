7324b5102948e488998384bd8188ae6037d00e6c
yaowenqiang <yaowenqiang111@163.com>
2015-03-14 03:59:02 +0800

    每个[list]元素中都带有两个参数的for循环

--- loop/planet.sh
+++ loop/planet.sh
@@ -15,4 +15,16 @@ do
     echo $planet
 done
 
+# 用行星距太阳的距离来分配行星的名字
+
+for planet in "Mercury 36" "Venus 67" "Earth 93" "Mars 142" "Jupiter 483"
+do
+    set -- $planet              # 解析变量 "$planet"并且设置位置参数
+    # "--"将阻止$planet为空，或者是以一个破折号开关。TODO 不是很懂
+    # 可能需要保存原始的位置参数，因为它们被覆盖了。
+    # 一种方法就是使用数组，
+    # original_param=("$@")
+    echo "$1            $2,000,000 miles from the sum"
+    #---------- two tabs----------把后边的和2连接起来
+done
 exit 0
