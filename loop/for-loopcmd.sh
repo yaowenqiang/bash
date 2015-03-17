16caa05a26979d8e4ec3cf907603b58a5b325f50
yaowenqiang <yaowenqiang111@163.com>
2015-03-18 02:17:06 +0800

    使用命令替换来产生for循环的[list]

---
+++ loop/for-loopcmd.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# for-loopcmd.sh        带[list]的for循环
+#+[list]是由命令替换产生的。
+
+NUMBERS="9 7 3 8 37.53"
+for number in $NUMBERS      # for number in 9 7 3 8 37.53
+do
+    echo -n "$number "
+done
+
+echo
+
+exit 0
+
