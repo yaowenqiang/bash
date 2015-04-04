be30aefbc32c8bb700c04042e06cf47b1489de30
jack yao <yaowenqiang111@163.com>
2015-04-04 13:20:15 +0800

    fix typo

--- outer-cmd/lookup.sh
+++ outer-cmd/lookup.sh
@@ -1,15 +1,15 @@
-#!bin/bash
+#!/bin/bash
 
 # lookup.sh; 对指定数据文件中的每个单词都做一遍字典查询。
 
-file=word.data          # 指定的要搜索的数据文件。
+file=words.data          # 指定的要搜索的数据文件。
 echo
 
 while [ "$word" !=  "end" ]         # 数据文件中最后一个单词
 do
     read word                       # 从数据文件中读，因为在循环的后面重定向了。
     look $word > /dev/null          # 不想将字典文件中的行显示出来。
-    lookup=$/                       # 'look'命令的退出状态.
+    lookup=$?                       # 'look'命令的退出状态.
 
     if [ "$lookup" -eq 0 ]
     then
