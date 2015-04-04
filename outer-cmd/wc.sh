abee4b9e3adfa7a61fc455813af49f39655ebce3
yaowenqiang <yaowenqiang111@163.com>
2015-04-04 13:34:22 +0800

    使用wc命令来统计所以有[d-h]开头的文件的大小

--- outer-cmd/wc.sh
+++ outer-cmd/wc.sh
@@ -9,3 +9,10 @@ ls *.txt  &2>/dev/null | wc -l
 # (shopt -s nullglob;set -- *.txt; echo $#)
 # TODO 好复杂，没完全看懂。
 
+# 统计所以有d-h开头的文件的大小。
+wc [d-z]* | grep total | awk '{print $3}'
+# 如果系统语言是中文的话要把total改为'总用量'
+wc [d-z]* | grep '总用量' | awk '{print $3}'
+# 或者直接取最后一行
+wc [d-z]* | tail -n 1 | awk '{print $3}'
+
