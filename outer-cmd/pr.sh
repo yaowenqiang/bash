48fd6ee6370a099966b13bad14527924ef9f96c6
yaowenqiang <yaowenqiang111@163.com>
2015-04-04 17:45:20 +0800

    添加了使用示例

--- outer-cmd/pr.sh
+++ outer-cmd/pr.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # pr.sh         功能非常的多，不过还没用过。主要用来格式化文本.
+# 集合了许多命令的功能，比如 nl,p0aste,fold,column,和expand
+# pr -o 5 --width=65 fileZZZ | more
+# 这个命令对filezzz进行了比较好的分页，并且打印到屏幕上，文件的缩进被设置为５，总宽度设置为65,一个非常有用的选项-d,强制隔行打印(与sed -G效果相同)
 # TODO
