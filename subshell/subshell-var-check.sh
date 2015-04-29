20948d05b5c786671623a06ff9ad4c9c66507f7a
jack yao <yaowenqiang111@163.com>
2015-04-30 01:08:54 +0800

    [32m《征人怨》[m [33m作者：柳中庸[m 岁岁金河复玉关，朝朝马策与刀环。 三春白雪归青冢，万里黄河绕黑山。

--- subshell/subshell-var-check.sh
+++ subshell/subshell-var-check.sh
@@ -24,7 +24,7 @@ if (set -u;: $variable) 2> /dev/null
 then
     echo "Variable is set. "
 fi # 变量已经在当前脚本中被设置，
-   # 或者是一个Bash的内建亦是。 
+   # 或者是一个Bash的内建变量。 
    # 或者是在当前环境下的一个可见变量(指已经被export的环境变量)
    # 也可以写成： [[ $variable-x != x || ${variable-x} != y ]]
    # 或： [[ ${variable-x} != x$variable ]]
