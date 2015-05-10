66b196e99ebd951414f5ae80fe75bc9695cc2625
jack yao <yaowenqiang111@163.com>
2015-05-10 23:41:19 +0800

    debug和调试，加注释,这个有好多还没理解

--- array/empty-array.sh
+++ array/empty-array.sh
@@ -20,6 +20,7 @@
 #set -o nounset                              # Treat unset variables as an error
 
 # 空数组与包含空元素的数组,这两个概念不同。
+# TODO 后面的要多看一下变量替换章节，http://www.21andy.com/manual/advanced-bash-scripting-guide/parameter-substitution.html
 array0=( first second third )
 array1=( '' )           # 'array1'包含一个空元素
 array2=( )              # 没有元素...,'array2'为空
@@ -28,8 +29,8 @@ listArray()
 {
     echo
     echo "Element in array0: ${array0[@]}"
-    echo "Element in array1: ${array0[@]}"
-    echo "Element in array2: ${array0[@]}"
+    echo "Element in array1: ${array1[@]}"
+    echo "Element in array2: ${array2[@]}"
     echo
     echo "Length of element in array0 is ${#array0}"
     echo "Length of element in array1 is ${#array1}"
@@ -42,7 +43,7 @@ listArray()
 
 # ================================================================================
 
-listArray
+#listArray
 
 # 尝试扩展这些数组。
 # 添加一个元素到这个数组。
@@ -50,23 +51,20 @@ array0=( "${array0[@]}" "new1" )
 array1=( "${array1[@]}" "new1" )
 array2=( "${array2[@]}" "new1" )
 
-listArray
-
+#listArray
 # 或
 
 array0[${#array0[*]}]="new2"
 array1[${#array1[*]}]="new2"
 array2[${#array2[*]}]="new2"
 
-listArray
-
+#listArray
 # 如果你按照上边的方法对数组进行扩展的话，数组比较像'栈'
 # 上边的操作就是'压栈'
 # 栈'高'为:
 height=${#array2[@]}
 echo
 echo "Stack height for array2 = $height"
-
 # "出栈"就是：
 unset array2[${#array2[@]}-1]           # 数组从0开始索引。
 height=${#array2[@]}                    # 这意味着每一个数组的下标为0
@@ -74,34 +72,37 @@ echo
 echo "POP"
 echo "New stack height for array2 = $height"
 echo
-listArray
+#listArray
 
 # 只列出数组array1的第二个和第三个元素。
 from=1                  # 从0开始索引
-to=2                    # 
-array3=( ${array0[@]:1:2} )
+to=2                    #
+array3=( ${array0[@]:1:2} ) # 从索引0开始，取2个元素，即取第2和第3个元素
 echo
 echo "Elements in array3: ${array3[@]}"
-
 # 处理方式就像是字符串(字符数组)
 # 试试其他的"字符串"形式
 
 # 替换:
-array4=( ${array[@]/second/2nd} )
+array4=( ${array0[@]/second/2nd} ) # 替换数组中开 头匹配的元素
 echo
 echo "Elements in array4: ${array4[@]}"
-
 # 替换所有匹配符的字符串
 array5=( ${array0[@]//new?/old} )
-echo "Elements in array5: ${array4[@]}"
+echo "Elements in array5: ${array5[@]}"
 
+# 跟上边的对比一下
+#array5_1=( ${array0[@]/d/D} )
+#echo "Elements in array5_1: ${array5_1[@]}"
 
+#array5_2=( ${array0[@]//d/D} )
+#echo "Elements in array5_2: ${array5_2[@]}"
 # 当你开始坚此有把握的时候，
 ARRAY6=( ${ARRAY0[@]#new} )        # TODO 这个#又忘记了
 echo                               # 这个可能会让你感到惊奇
 echo "Elements in array6: ${array6[@]}"
 
-array7=( ${array0[@]#new1} )
+array7=( ${array0[@]#new1} ) # 从开头删除最短匹配
 echo                               # 数组array6之后就没有惊奇了
 echo "Elements in array7: ${array7[@]}"
 
@@ -119,7 +120,7 @@ echo "Elements in array8: ${array8[@]}"
 # 元素会在结果赋值中消失不见，
 # 一个问题，这些字符串是强引用还是弱引用？TODO
 zap="new*"
-array9=( ${array0[@]/$rap} )
+array9=( ${array0[@]/$zap} )
 echo
 echo "Elements in array9: ${array9[@]}"
 
@@ -132,5 +133,5 @@ echo "Elements in array10: ${array10[@]}"
 # 比较array8和array9,
 
 # 答案：必须是弱引用 。
-
+# TODO 弱类型和强类型没懂有什么区别,
 exit 0
